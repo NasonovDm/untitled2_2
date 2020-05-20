21b7d5d4292ab572e64a4bf57e763f9a1e28584e
Nasonov Dmitriy <onlydmitriy@gmail.com>
2020-05-21 02:11:55 +0300

    Initial commit

---
+++ src/dz2_2/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package dz2_2;
+
+public class MyArrayDataException extends RuntimeException {
+    public MyArrayDataException(String message) {
+        super("Некорректные данные "+message);
+    }
+}
